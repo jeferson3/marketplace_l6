7bf62c1bde5172df1d645c33873a8354083fde66
jeferson <jefersongomes333@hotmail.com>
2020-07-03 20:20:01 -0300

    Update Store.php

--- app/Store.php
+++ app/Store.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Store extends Model //procura por uma tabela chamada stores
 {
     // protected $table = 'NomeDaTabela'; //aqui pode mudar o nome da tabela ao qual o model está associado;
+
+
+
+    public function user(){
+        return $this->belongsTo(User::class); //relacionamento, uma loja pertence a um user
+    }
+
 }
