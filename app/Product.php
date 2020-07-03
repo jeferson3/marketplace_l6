b2afac633b8b858e175d869cb2027a31e9017402
jeferson <jefersongomes333@hotmail.com>
2020-07-03 20:39:30 -0300

    Update Product.php

--- app/Product.php
+++ app/Product.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model //procura por uma tabela chamada products
 {
     // protected $table = 'NomeDaTabela'; //aqui pode mudar o nome da tabela ao qual o model está associado;
-    protected $fillable = ['id_store', 'name', 'description', 'price', 'slug', 'body'];
+    protected $fillable = ['name', 'description', 'price', 'slug', 'body'];
 
 
     public function store()
@@ -15,6 +15,9 @@ class Product extends Model //procura por uma tabela chamada products
         return $this->belongsTo(Store::class);
     }
 
-
+    public function categorie()
+    {
+        return $this->belongsToMany(Category::class);  //relacionamento N:N, pertence a varias categorias
+    }
 
 }
