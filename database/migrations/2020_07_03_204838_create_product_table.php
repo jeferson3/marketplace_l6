c08497a3cc34a2bdc49d1ed8952f52264a3fe40f
jeferson <jefersongomes333@hotmail.com>
2020-07-03 17:57:17 -0300

    table product

---
+++ database/migrations/2020_07_03_204838_create_product_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('id_store');
+            $table->string('name');
+            $table->string('description');
+            $table->string('body');
+            $table->decimal('price', 10, 2);
+            $table->string('slug');
+
+            $table->foreign('id_store')->references('id')->on('stores');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
