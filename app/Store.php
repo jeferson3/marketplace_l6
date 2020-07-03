9629265a44a24dbfc075e7f77eee6886b50f587f
jeferson <jefersongomes333@hotmail.com>
2020-07-03 20:39:33 -0300

    Update Store.php

--- app/Store.php
+++ app/Store.php
@@ -8,7 +8,9 @@ class Store extends Model //procura por uma tabela chamada stores
 {
     // protected $table = 'NomeDaTabela'; //aqui pode mudar o nome da tabela ao qual o model está associado;
 
-
+    protected $fillable = [
+        'name', 'description', 'phone','moble_phone','slug'
+    ];
 
     public function user(){
         return $this->belongsTo(User::class); //relacionamento, uma loja pertence a um user
