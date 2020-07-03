f6d4d97eff615171af498406f0177e1f5ae1a0fb
jeferson <jefersongomes333@hotmail.com>
2020-07-03 19:11:59 -0300

    Update Product.php

--- app/Product.php
+++ app/Product.php
@@ -4,7 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Product extends Model //procura por uma tabela chamada products
 {
-    //
+    // protected $table = 'NomeDaTabela'; //aqui pode mudar o nome da tabela ao qual o model está associado;
+    protected $fillable = ['id_store', 'name', 'description', 'price', 'slug', 'body'];
 }
