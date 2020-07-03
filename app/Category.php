08998661369d082d3f2165d508d0fc071b636d49
jeferson <jefersongomes333@hotmail.com>
2020-07-03 20:39:27 -0300

    Update Category.php

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'description', 'slug'
+    ];
+    
+    public function product()
+    {
+        return $this->belongsToMany(Product::class); //relacionamento N:N,pertence a varios produtos
+    }
 }
