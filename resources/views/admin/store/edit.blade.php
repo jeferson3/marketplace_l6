11669895141e786befb79ef4ed7d2e2cc35d6f68
jeferson <jefersongomes333@hotmail.com>
2020-07-04 18:36:03 -0300

    Create edit.blade.php

---
+++ resources/views/admin/store/edit.blade.php
@@ -0,0 +1,40 @@
+@extends('template.app')
+@section('title', 'Edit Store')
+
+@section('root')
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <form action="{{ route('store.update', $store->id) }}" method="POST">
+            @csrf
+            @method('PUT')
+            
+            <div  class="form-group">
+                <label for="name">Nome</label>
+                <input type="text" class="form-control" name="name" id="name" value="{{ $store->name }}">
+            </div>
+            <div  class="form-group">
+                <label for="description">Descrição</label>
+                <input type="text" class="form-control" name="description" id="description" value="{{ $store->description }}">
+            </div>
+            <div  class="form-group">
+                <label for="mobile_phone">Celular</label>
+                <input type="text" class="form-control" name="mobile_phone" id="mobile_phone" value="{{ $store->mobile_phone }}">
+            </div>
+            <div  class="form-group">
+                <label for="phone">Telefone</label>
+                <input type="text" class="form-control" name="phone" id="phone" value="{{ $store->phone }}">
+            </div>
+            <div  class="form-group">
+                <label for="slug">Slug</label>
+                <input type="text" class="form-control" name="slug" id="slug" value="{{ $store->slug }}">
+            </div>
+            
+            <div  class="form-group">
+                <button type="submit" class="btn btn-outline-primary">Atualizar loja</button>
+            </div>
+        </form> 
+    </div>
+    </div>
+    <div class="row justify-content-center">
+    </div>
+@endsection
