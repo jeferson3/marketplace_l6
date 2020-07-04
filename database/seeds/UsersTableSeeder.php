706ed8a26e4b8aba1e099d2f278fa0dda947d432
jeferson <jefersongomes333@hotmail.com>
2020-07-03 21:30:07 -0300

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Store;
 use App\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -15,16 +16,17 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
 
-        DB::table('users')->insert([
+        // DB::table('users')->insert([
+        //     'name' => 'Administrador',
+        //     'email' => 'admin@email.com',
+        //     'email_verified_at' => now(),
+        //     'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+        //     'remember_token' => 'mkcdnkvndkvnkdnv',
+        // ]); //adiciona um user no banco
 
-            'name' => 'Administrador',
-            'email' => 'admin@email.com',
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-            'remember_token' => 'mkcdnkvndkvnkdnv',
-        ]); //adiciona um user no banco
-
-        factory(User::class, 10)->create();
+        factory(User::class, 40)->create()->each(function($user){
+            $user->store()->save(factory(Store::class)->make());
+        });
     
     }
 }
