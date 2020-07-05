9f536c40196efdf868fbbf21aa75deea3c8e158f
jeferson <jefersongomes333@hotmail.com>
2020-07-05 14:21:25 -0300

    Update ProductController.php

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -4,10 +4,21 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Product;
+use App\Store;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    private $request;
+    private $product;
+
+    public function __construct(Request $request, Product $product)
+    {
+        $this->request = $request;
+        $this->product = $product;
+    }
+
+
     /**
      * Display a listing of the resource.
      *
@@ -15,7 +26,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = $this->product::paginate(10);
+        return view('admin.product.index', ['products' => $products]);
     }
 
     /**
@@ -25,62 +37,72 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        $stores = Store::all(['id', 'name']);
+        return view('admin.product.create', ['stores' => $stores]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store()
     {
-        //
+        $product = $this->request->all();
+        $store = Store::findOrFail($product['store']);
+        $store->product()->create($product);
+
+        flash('Produto criado com sucesso')->success();
+        return redirect()->route('products.index');
     }
 
     /**
      * Display the specified resource.
-     *
-     * @param  \App\Product  $product
+     * @param $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Product $product)
+    public function show($id)
     {
-        //
+        $product = $this->product::findOrFail($id);
+        return view('admin.product.show', ['produc' => $product]);
     }
 
     /**
      * Show the form for editing the specified resource.
-     *
-     * @param  \App\Product  $product
+     * @param $id
      * @return \Illuminate\Http\Response
      */
-    public function edit(Product $product)
+    public function edit($id)
     {
-        //
+        $product = $this->product->findOrFail($id);
+        return view('admin.product.edit', ['product' => $product]);
     }
 
     /**
      * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Product  $product
+     * @param $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Product $product)
+    public function update($id)
     {
-        //
+        $product = $this->product::findOrFail($id);
+        $product->update($this->request->all());
+        flash('Produto atualizado com sucesso')->success();
+
+        return redirect()->route('products.index');
     }
 
     /**
      * Remove the specified resource from storage.
-     *
-     * @param  \App\Product  $product
+     * @param $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Product $product)
+    public function destroy($id)
     {
-        //
+        $product = $this->product::findOrFail($id);
+        $product->delete();
+        flash('Produto deletado com sucesso')->success();
+
+        return redirect()->route('products.index');
     }
 }
