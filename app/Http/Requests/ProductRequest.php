af03ca27d9ae028d6916d32519969e899a00e489
jeferson <jefersongomes333@hotmail.com>
2020-07-06 13:25:41 -0300

    Create ProductRequest.php

---
+++ app/Http/Requests/ProductRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'min:3'],
+            'description' => ['required', 'min:3'],
+            'price' => ['required'],
+            'body' => ['required', 'min:10'],
+            'slug' => ['required'],
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'required' => 'Este campo é obrigatório',
+            'min' => 'Este campo deve ter no mínimo :min caracteres',
+        ];
+    }
+}
