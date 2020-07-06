7c8543d0026ee19356f4b487cc650939526154a1
jeferson <jefersongomes333@hotmail.com>
2020-07-06 13:25:49 -0300

    Update create.blade.php

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -1,38 +1,63 @@
 @extends('template.app')
 @section('title', 'Create Store')
 @section('root')
+
+    <div class="row">
+        <a href="{{ url()->previous() }}" style="text-decoration: none;">
+            <i class="fas fa-arrow-left my-auto mx-2"></i>
+            Voltar
+
+        </a>
+    </div>
     <h1>Criar Produto</h1>
     <form action="{{ route('products.store') }}" method="POST">
         @csrf
         
         <div  class="form-group">
             <label for="name">Nome</label>
-            <input type="text" class="form-control" name="name" id="name">
+            <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name">
+            @error('name')
+                <small class="invalid-feedback">
+                    {{ $message }}
+                </small>
+            @enderror
         </div>
         <div  class="form-group">
             <label for="description">Descrição</label>
-            <input type="text" class="form-control" name="description" id="description">
+            <input type="text" class="form-control @error('name') is-invalid @enderror" name="description" id="description">
+            @error('description')
+                <small class="invalid-feedback">
+                    {{ $message }}
+                </small>
+            @enderror
         </div>
         <div  class="form-group">
             <label for="price">Preço</label>
-            <input type="text" class="form-control" name="price" id="price">
+            <input type="text" class="form-control @error('name') is-invalid @enderror" name="price" id="price">
+            @error('price')
+                <small class="invalid-feedback">
+                    {{ $message }}
+                </small>
+            @enderror
         </div>
         
         <div  class="form-group">
             <label for="slug">Slug</label>
-            <input type="text" class="form-control" name="slug" id="slug">
+            <input type="text" class="form-control @error('name') is-invalid @enderror" name="slug" id="slug">
+            @error('slug')
+                <small class="invalid-feedback">
+                    {{ $message }}
+                </small>
+            @enderror
         </div>
         <div  class="form-group">
             <label for="body">Conteúdo</label>
-            <textarea type="text" rows="5" class="form-control" name="body" id="body"></textarea>
-        </div>
-        <div  class="form-group">
-            <label for="store">Usuário</label>
-            <select name="store" class="form-control" id="store">
-                @foreach ($stores as $store)
-                    <option value="{{ $store->id }}">{{ $store->name }}</option>
-                @endforeach
-            </select>
+            <textarea type="text" rows="5" class="form-control @error('name') is-invalid @enderror" name="body" id="body"></textarea>
+            @error('body')
+                <small class="invalid-feedback">
+                    {{ $message }}
+                </small>
+            @enderror
         </div>
         <div  class="form-group">
             <button type="submit" class="btn btn-outline-primary">Criar produto</button>
