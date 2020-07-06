8945eedc7b257a0d610208d226734e86da999ae3
jeferson <jefersongomes333@hotmail.com>
2020-07-06 19:34:06 -0300

    Create create.blade.php

---
+++ resources/views/admin/category/create.blade.php
@@ -0,0 +1,49 @@
+@extends('template.app')
+@section('title', 'Create Category')
+@section('root')
+
+    <div class="row">
+        <a href="{{ url()->previous() }}" style="text-decoration: none;">
+            <i class="fas fa-arrow-left my-auto mx-2"></i>
+            Voltar
+
+        </a>
+    </div>
+    <h1>Criar Categoria</h1>
+    <form action="{{ route('categories.store') }}" method="POST">
+        @csrf
+        
+        <div  class="form-group">
+            <label for="name">Nome</label>
+            <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name">
+            @error('name')
+                <small class="invalid-feedback">
+                    {{ $message }}
+                </small>
+            @enderror
+        </div>
+        <div  class="form-group">
+            <label for="description">Descrição</label>
+            <input type="text" class="form-control @error('description') is-invalid @enderror" name="description" id="description">
+            @error('description')
+                <small class="invalid-feedback">
+                    {{ $message }}
+                </small>
+            @enderror
+        </div>
+        
+        <div  class="form-group">
+            <label for="description">Slug</label>
+            <input type="text" class="form-control @error('slug') is-invalid @enderror" name="slug" id="slug">
+            @error('slug')
+                <small class="invalid-feedback">
+                    {{ $message }}
+                </small>
+            @enderror
+        </div>
+
+        <div  class="form-group">
+            <button type="submit" class="btn btn-outline-primary">Criar categoria</button>
+        </div>
+    </form>
+@endsection
