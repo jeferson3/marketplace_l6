0298451518f0b2defaf018276a776e17a5e4355a
jeferson <jefersongomes333@hotmail.com>
2020-07-08 12:37:25 -0300

    Update Store.php

--- app/Store.php
+++ app/Store.php
@@ -3,18 +3,31 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Store extends Model //procura por uma tabela chamada stores
 {
     // protected $table = 'NomeDaTabela'; //aqui pode mudar o nome da tabela ao qual o model está associado;
 
+    
     protected $fillable = [
         'name', 'description', 'phone','mobile_phone','slug', 'logo'
     ];
+    
+    use HasSlug;
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
 
     public function user(){
         return $this->belongsTo(User::class); //relacionamento, uma loja pertence a um user
     }
+    
     public function product()
     {
         return $this->hasMany(Product::class);
