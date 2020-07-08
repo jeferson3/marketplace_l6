33adf8626dd00080d46b11e96d8e2ac15acce640
jeferson <jefersongomes333@hotmail.com>
2020-07-08 12:36:42 -0300

    Update Category.php

--- app/Category.php
+++ app/Category.php
@@ -3,13 +3,23 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Category extends Model
 {
     protected $fillable = [
         'name', 'description', 'slug'
     ];
-    
+    use HasSlug;
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+
     public function product()
     {
         return $this->belongsToMany(Product::class); //relacionamento N:N,pertence a varios produtos
