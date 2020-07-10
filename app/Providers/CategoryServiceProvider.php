5e36484018ad6ecc55f2b470c80cfc31c867713c
jeferson <jefersongomes333@hotmail.com>
2020-07-10 11:31:50 -0300

    Create CategoryServiceProvider.php

---
+++ app/Providers/CategoryServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CategoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+        ## COMPARTILHAR DADOS EM TODAS AS VIEWS ##
+
+        // $categories = Category::all(['name', 'slug']);
+        // view()->composer('*', function($view) use($categories){
+        //     $view->with('categories', $categories);
+        // });
+
+        view()->composer('*', 'App\Http\Views\CategoryViewComposer@compose');
+    }
+}
