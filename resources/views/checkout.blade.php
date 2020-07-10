f6f5271a2bcde4372eda96f41cc5cd60fe746868
jeferson <jefersongomes333@hotmail.com>
2020-07-09 21:17:23 -0300

    Create checkout.blade.php

---
+++ resources/views/checkout.blade.php
@@ -0,0 +1,60 @@
+@extends('template.app')
+
+@section('root')
+    <div class="container">
+        <div class="col-md-12">
+            <form class="form" action="{{route('checkout.order')}}" method="POST">
+                @csrf
+                <div class="row justify-content-center">
+                    <div class="col-md-10">
+                        <h2>Dados pagamento</h2>
+                        <p class="text-danger">Metodo de pagamento não disponível</p>
+                        <hr>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-md-10 form-group">
+                        <label for="">Número do cartão</label>
+                        <input type="text" class="form-control" name="card_number">
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-md-5 form-group">
+                        <label for="">Mês de Expiração</label>
+                        <input type="text" class="form-control" name="card_month">
+                    </div>
+                    <div class="col-md-5 form-group">
+                        <label for="">Ano de Expiração</label>
+                        <input type="text" class="form-control" name="card_year">
+                    </div>
+                </div>
+                <div class="row justify-content-center align-items-end">
+                    <div class="col-md-5">
+                        <label for="">Código de Segurança</label>
+                        <input type="text" class="form-control" name="card_cvv">
+                    </div>
+                    <div class="col-md-5 mt-2">
+                        <button type="submit" class="btn btn-success payment">Efetuar pagamento</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+@endsection
+@section('scripts')
+    <script>
+        document.querySelector('.form')
+        .addEventListener('submit', function(e) {
+
+            e.preventDefault();
+
+            setTimeout(function() {
+                
+                document.querySelector('.form').submit();
+            }, 3000)
+
+            toastr.success('Compra realizada com sucesso', 'Sucesso', {timeOut: 3000})
+        });
+    </script>
+@endsection
