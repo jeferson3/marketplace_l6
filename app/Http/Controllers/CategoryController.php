92b32c36578d8972b2ac2951ec11c2ed8b3b4279
jeferson <jefersongomes333@hotmail.com>
2020-07-10 11:31:40 -0300

    Create CategoryController.php

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    private $category;
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+    }
+    public function index($slug)
+    {
+        $category = $this->category->whereSlug($slug)->first();
+        // dd($category);
+        return view('category', ['category'=>$category]);
+    }
+}
