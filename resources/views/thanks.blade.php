91875e433903bb4c9849d48a39f27fcb76cb8e23
jeferson <jefersongomes333@hotmail.com>
2020-07-09 21:17:33 -0300

    Create thanks.blade.php

---
+++ resources/views/thanks.blade.php
@@ -0,0 +1,14 @@
+@extends('template.app')
+
+@section('root')
+    <div class="container">
+        <div class="alert alert-success alert-important">
+            <h2>Obrigado pela compra</h2>
+        </div>
+        <hr>
+        <h3>Pedido: 54164211849266AB</h3>
+        <hr>
+        <a href="{{route('home')}}">Home</a>
+    </div>
+
+@endsection
