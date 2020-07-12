1626ba3d49323734af82af5f08a239c283793112
jeferson <jefersongomes333@hotmail.com>
2020-07-12 12:32:03 -0300

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,7 +61,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'user.has.store' => \App\Http\Middleware\UserHasStore::class
+        'user.has.store' => \App\Http\Middleware\UserHasStore::class,
+        'user.is.admin' => \App\Http\Middleware\AccessControlStoreAdmin::class
     ];
 
     /**
